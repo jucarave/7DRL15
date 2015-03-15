fd7427ad477df0549bbd6c0f98a0e14f6806a723
Camilo Ramírez <jucarave92@hotmail.com>
2015-03-14 22:00:59 -0500

    no test map

--- web/js/SelectClass.js
+++ web/js/SelectClass.js
@@ -34,7 +34,7 @@ SelectClass.prototype.step = function(){
 		
 		if (playerS.virtue != null){
 			game.createInitialInventory();
-			game.loadMap('test', 1);
+			game.loadMap(false, 1);
 		}
 	}
 };
