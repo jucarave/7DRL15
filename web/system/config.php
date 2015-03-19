f0438b32d22145cdf11ceb3f2b6d48aac53c1b62
slashman <java.koder@gmail.com>
2015-03-18 19:05:40 -0500

    Version 7drlbf2

--- web/system/config.php
+++ web/system/config.php
@@ -1,10 +1,10 @@
 <?php
-	$version = "A0.1";
-	$environment = "LOCAL";
+	$version = "7drlbf2";
+	$environment = "SERVER";
 	$contextPath = "/7DRL15/";
 	
 	if ($environment == "SERVER"){
-		$contextPath = "/games/7DRL15/"; 
+		$contextPath = "/stygian/play/"; 
 		
 		if ($_SERVER['HTTP_HOST'] == "games.jucarave.net" || $_SERVER['HTTP_HOST'] == "games.jucarave.com")
 			$contextPath = "/7DRL15/";
